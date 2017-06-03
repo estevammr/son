e89c297dc9723fefa64dda325b0776fa63453fac
Estevam de Moraes Rodrigues <estevam.mrodrigues@gmail.com>
2017-06-03 13:14:31 -0300

    Crud iniciado

--- vuejs/curso-son/src/router/index.js
+++ vuejs/curso-son/src/router/index.js
@@ -1,11 +1,16 @@
 import Hello from '@/components/Hello'
+import AccountsList from '../components/accounts/List'
+import AccountsView from '../components/accounts/View'
+import AccountsCreate from '../components/accounts/Create'
+import AccountsEdit from '../components/accounts/Edit'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Hello',
-    component: Hello
-  }
+  { path: '/', name: 'Hello', component: Hello },
+  { path: '/contas', component: AccountsList },
+  { path: '/contas/novo', component: AccountsCreate },
+  { path: '/contas/:id', component: AccountsView },
+  { path: '/contas/:id/editar', component: AccountsEdit }
+
 ]
 
 export default routes
