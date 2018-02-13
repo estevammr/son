b163a325c0b505dba7236fe37848ce4e4e332933
Estevam Rodrigues <erodrigues@localhost.localdomain>
2018-02-13 18:47:28 -0200

    Course design patterns

---
+++ design-patterns-php/src/QueryBuilder/Sql.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SON\Db\QueryBuilder;
+
+class Sql implements Strategy
+{
+    public function table(string $table) : Strategy
+    {
+        
+    }
+    public function select($columns = '*') : Strategy 
+    {
+
+    }
+    public function getQuery() : string
+    {
+
+    }
+}
